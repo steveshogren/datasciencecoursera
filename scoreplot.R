8b8338c5eb92768d81d5fc31ad84187c36eff7f1
Steve Shogren <steve.a.shogren@gmail.com>
2015-07-25 15:43:15 -0400

    All the safety score chart code

--- scoreplot.R
+++ scoreplot.R
@@ -1,44 +1,74 @@
+library(qcc)
+
+
 normalize <- function(x) {
-    return (x - min(x) / (max(x) - min(x))
+    return (x - min(x)) / (max(x) - min(x))
   }
+rescale <- function(x) (x-min(x))/(max(x) - min(x)) * 100
+invert <- function(x) x*-1
+
+bugratios <- rescale(c(0.039445132,0.047242288,0.020303702,0.02531419,0.031669293,0.03261284,0.011503478,0.024698375,0.032567736,0.01904762,0.023486288,0.015551204))
+names <- c('js','coffeescript','ruby','python','php','csharp','clojure','go','java','scala','fsharp','haskell')
+minbugratios <- rescale(c(0.021691974,0.017793594,0.006535948,0.0076045627,0.013927577,0.00887199,0.0057077627,0.0093896715,0.0125,0.007863696,0.0114068445,0.005192878))
+maxbugratios <- rescale(c(0.06540623,0.062305298,0.033620946,0.047004607,0.047918685,0.068942875,0.020320855,0.04109589,0.05942623,0.04706641,0.02955665,0.032920726))
+newscores <- rescale(invert(c(-3,-2,-2,-2,-1,3,3,3,3,6,9,10)))
+repos <- rescale(c(1475201,49716,826600,721979,630816,307572,30257,75802,1224939,49937,4180,37328))
+
+
+setwd("/home/jack/programming/datasciencecoursera")
+jpeg('errorChart.jpg')
+df <- read.csv("langugagedata2.csv", colClasses = "numeric")
+boxplot(df, las=2)
+dev.off()
+
+
+cor(bugratios, newscores)
+cor(repos, newscores)
+cor(repos, bugratios)
+
+df <- data.frame("mins" = minbugratios,"maxs" = maxbugratios, "bugs"=bugratios, stringsAsFactors = FALSE)
+boxplot(df)
 
 chartit <-  function(df){
-  plot(df$old, type="o", col="blue", axes=FALSE, ann=FALSE);
-  lines(df$new, type="o", pch=22, lty=2, col="red");
-  lines(df$bugs, type="o", pch=22, lty=2, col="green");
-  axis(1, at=1:5, lab=c("C#","F#","Clojure","Javascript", "Coffeescript")
+  plot(df$new, type="o", col="red", axes=FALSE, ann=FALSE);
+  lines(df$bugs, type="o", pch=22, lty=2, col="blue");
+  title("Safety Scores And Bugs/Commit")
+  par(las=2)
+  axis(1, at=1:12, lab= names)
   box()
-  title(xlab="Language", col.lab=rgb(0,0,0)
-  title(ylab="Score Magnitude", col.lab=rgb(0,0, 0)
-  legend("topright", NULL, c("overfitted model","simpler model","av. bug/repo"), cex=0.8,
-         col=c("blue","red", "green"), pch=21:22, lty=1:2);
+  title(ylab="Magnitude In Set", col.lab=rgb(0,0,0))
+  legend("topright", NULL, c("Bugs Per Commit","Inverted Safety Score"), cex=0.8,
+         col=c("blue","red"), pch=21:22, lty=1:2);
 }
 
-## trimmed mean
+setwd("/home/jack/programming/datasciencecoursera")
+jpeg('bothMags.jpg')
+df <- data.frame("repos" = repos,"new" = newscores, "bugs"=bugratios, stringsAsFactors = FALSE)
+chartit(df)
+dev.off()
 
-## oldscores <- c(-70, 192, 24, -187, -115);
-## newscores <- c(2, 8, 3, -3, -2);
-## bugsperproj <- c(-107.43333,  -16.80000,  -10.48333, -101.25000,  -58.46667)
-## bugsperproj <- c(-10.906667, -0.85714287, -1.3981482, -14.886666, -5.48)
-## bugsperproj <- c(-32, -20, -3, -44, -23)
-## bugsperproj <- c(-24, -3, -2, -44, -23)
-## bugsperproj <- c(-15, -4, -3, -29, -7)
-## oldscores <-   c(0,0,0,0,0, 0,0,0,0,0,0,0);
-newscores <- c(2, 8, 3, -3, -2,  0,0,0,0,0,0,0);
-oldscores <- c(-15,-4,-3,-29,-7,-7,-22,-13,-23,-5,-6,-12)
-bugsperproj <- c(63,45,33,123,35,30,65,48,60,23,17,44)
+chartQCC <- function(dt, name){
+  m <- mean(dt)
+  plot(dt, type="o", col="red", axes=FALSE, ann=FALSE);
+  title(name)
+  abline(h=m, col="blue");
+  par(las=2)
+  axis(1, at=1:12, lab= names)
+  axis(2)
+  box()
+  legend("topright", NULL, c("Bugs Per Commit","Mean"), cex=0.8,
+         col=c("red","blue"), pch=21:22, lty=1:2);
+}
 
-        
+setwd("/home/jack/programming/datasciencecoursera")
+jpeg('bugsAverage.jpg')
+chartQCC(c(0.039445132,0.047242288,0.020303702,0.02531419,0.031669293,0.03261284,0.011503478,0.024698375,0.032567736,0.01904762,0.023486288,0.015551204), "Bugs Per Commit Sorted By Safety Score")
+dev.off()
 
-df <- data.frame( "old" = oldscores, "new" = newscores, "bugs"=bugsperproj, stringsAsFactors = FALSE)
-dfNorm <- as.data.frame(lapply(df, normalize)
-
-
-## setwd("/home/jack/programming/datasciencecoursera")
-## jpeg('rplot.jpg')
-chartit(dfNorm)
-## dev.off()
-
+setwd("/home/jack/programming/datasciencecoursera")
+jpeg('scoreAverage.jpg')
+chartQCC(c(-3,-2,-2,-2,-1,3,3,3,3,6,9,10), "Safety Score")
+dev.off()
 
 
 bugs <- c(6446, 588, 629, 6075, 3508)
@@ -116,6 +146,22 @@ cor(pythonbugs, pythontests)
 ## [1] 0.07100754
 
 
-newscores <- c(2, 8, 3, -3, -2) 
-bugsPerCommit <- c(63,45,33,123,35)
+newscores <- c(2,9,3,-3,-2,6,-1,2,1,8,-2,-2)
+bugsPerCommit <- c(63,45,33,123,35,30,65,48,60,23,17,44)
 cor(newscores, bugsPerCommit)
+
+
+chartQCC <- function(dt){
+    my.xmr.raw <- dt
+    x <- qcc(my.xmr.raw, type = "xbar.one", title = "Individuals Chart\nfor Wheeler sample data")
+    x <- qcc(my.xmr.raw, ncol = 2, type = "R", title = "Moving Range Chart\nfor Wheeler sample data")
+    x
+}
+
+setwd("/home/jack/programming/datasciencecoursera")
+jpeg('rmean_bugs.jpg')
+df <- invert(c(63,45,33,123,35,30,65,48,60,23,17,44))
+jpeg('rmean_score.jpg')
+df <- c(2,9,3,-3,-2,6,-1,2,1,8,-2,-2)
+chartQCC(df)
+dev.off()
